de88a8025df25679959ecdc49f25966af0b43a79
Daniel Interiano <dannyboyint@gmail.com>
2022-04-25 20:39:09 -0400

    added validations to my models, and testing them out

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,8 +1,6 @@
 class Plant < ApplicationRecord
     has_many :reviews
     
-    #validates :name, :species, :sun_exposure, :soil_type, :water_cycle, :bio, :image_url, presence: true
-    #validates :bio, length:{maximum: 100}
-    
-
+    validates :name, :species, :sun_exposure, :soil_type, :water_cycle, :bio, :image_url, presence: true
+    validates :bio, length:{maximum: 100}
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
-    
     belongs_to :plant     
+
+    validates :title, :comment, :score, presence: true
+    validates :comment, length: {maximum: 100}
+    
 end
